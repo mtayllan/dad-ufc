ca490dab4bbac7d0608134dd16a64bc4414fadb7
mtayllan <messiastayllan@gmail.com>
2022-06-27 20:37:20 -0300

    update get_image envs

--- api/get_image.rb
+++ api/get_image.rb
@@ -6,13 +6,12 @@ require 'securerandom'
 module GetImage
   @aws_lambda = Aws::Lambda::Client.new(
     region: 'us-east-1',
-    endpoint: ENV['LAMBDA_ENDPOINT'],
-    credentials: Aws::Credentials.new(ENV['LAMBDA_AKI'], ENV['LAMBDA_SAK'])
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   )
 
   @aws_s3 = Aws::S3::Client.new(
     region: 'us-east-1',
-    credentials: Aws::Credentials.new(ENV['S3_AKI'], ENV['S3_SAK'])
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
   )
 
   def self.call(html)
@@ -20,11 +19,11 @@ module GetImage
       function_name: ENV['LAMBDA_ARN'],
       payload: { html: }.to_json
     })
-    base64_image = JSON.parse(lambda_response[:payload].string)['data']
+
+    base64_image = JSON.parse(lambda_response[:payload].string)['base64']
     filename = "#{SecureRandom.hex(16)}.png"
 
     object = @aws_s3.put_object({
-      acl: 'public-read',
       body: Base64.decode64(base64_image),
       bucket: ENV['S3_BUCKET'],
       key: filename
